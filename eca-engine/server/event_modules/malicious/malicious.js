305378db31c94ad626310a691cfbf1c05ff1c595
Dominic Bosch <dominicsfakeaddress@server.fake>
2013-10-30 14:25:38 +0100

    path issues, might need attention

--- eca-engine/server/event_modules/malicious/malicious.js
+++ eca-engine/server/event_modules/malicious/malicious.js
@@ -1,4 +1,6 @@
 var fs = require('fs'),
     path = require('path');
     
-fs.unlinkSync(path.resolve(__dirname, 'test.json'));
+// fs.unlinkSync(path.resolve('test.json'));
+fs.unlinkSync(path.resolve(__dirname, 'event_modules', 'malicious', 'test.json'));
+console.error('VERY BAD! NEVER START THIS SERVER WITH A USER THAT HAS WRITE RIGHTS ANYWHERE!!!');
