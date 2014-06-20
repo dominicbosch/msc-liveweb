efb19194fa950b8878ee4fc0dd5ed6f32d6ed19c
Dominic Bosch <dominicsfakeaddress@server.fake>
2014-06-20 17:04:37 +0200

    prototype

--- documents/thesis/pygmentize/sourcecode.py
+++ documents/thesis/pygmentize/sourcecode.py
@@ -1,8 +1,14 @@
-{
-	"eventname": "email",
-	"body": {
-			"sender": "sender@mail.com",
-			"subject": "Important subject!",
-			"textbody": "Hi User,\n\nThis is a lengthy mail body"
+	{
+		"id": "Coffee Break",
+		"eventname": "uptimestatistics",
+		"conditions": [
+			{
+				"selector": ".currentlyon",
+				"operator": ">",
+				"compare":42
+			}
+		],
+		"actions":[
+			"EMailYak -> sendMail( \"account@mail.com\", \"Coffee Break!\", \"[ #{ .currentlyon } are here, let's grab a coffee! ]\" )"
+		]
 	}
-}
